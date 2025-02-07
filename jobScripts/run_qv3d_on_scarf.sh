6490e962f78b7cf9edd9940801254c7069c9a5d3
Hossein Saberi <scarf1426@cn1108.scarf.rl.ac.uk>
2025-02-07 17:54:39 +0000

    modify slurm job run to calculate the run time

--- jobScripts/run_qv3d_on_scarf.sh
+++ jobScripts/run_qv3d_on_scarf.sh
@@ -20,10 +20,27 @@
 
 
 
-# Run the program:
-# ###############
 
+
+# Load the modules:
+# #################
 module load FFTW.MPI/3.3.10-gompi-2023b
 module load HDF5/1.14.3-gompi-2023b
 
+# Record the start time
+# #####################
+echo "Job started at: $(date)" > job_times_${SLURM_JOB_ID}.txt
+
+# Run QV3D job on clsuter
+# #######################
 srun -n  $SLURM_NTASKS ./qv3dMPIX.e v.ini
+
+# Record the end time
+# ###################
+echo "Job ended at: $(date)" >> job_times-${SLURM_JOB_ID}.txt
+
+# Calculate and record the run time
+start_time=$(date -d "$(head -n 1 job_times_${SLURM_JOB_ID}.txt | cut -d ' ' -f 4-)" +%s)
+end_time=$(date -d "$(tail -n 1 job_times_${SLURM_JOB_ID}.txt | cut -d ' ' -f 4-)" +%s)
+run_time=$((end_time - start_time))
+echo "Job run time: ${run_time} seconds" >> job_times-${SLURM_JOB_ID}.txt
